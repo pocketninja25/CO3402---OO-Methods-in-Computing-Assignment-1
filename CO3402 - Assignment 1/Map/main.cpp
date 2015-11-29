114eb85a6ed6c1d53a9b90a45da701d67c318e90
Daniel Reynolds <cybers_25@hotmail.co.uk>
2015-11-29 12:31:25 +0000

    Changed to do both tests in one run, without changing outputs

--- CO3402 - Assignment 1/Map/main.cpp
+++ CO3402 - Assignment 1/Map/main.cpp
@@ -8,29 +8,21 @@ using std::endl;
 using std::string;
 using std::ofstream;
 
-#define MyMapType	//Keep this commented in to use my implementation, comment it out to use the stl implementation //May have to use ifdefs elsewhere should my implementation diverge
 //#define ConsoleOut
 
-//Decides which map to use based on if CMAP is defined
-#ifdef MyMapType
-	#include "CMap.h"
-	#define MAP CMap
-#else
-	#include <map>
-	using std::map;
-	using std::make_pair;
-	#define MAP map
-#endif
+#include "CMap.h"
+#include <map>
+using std::map;
 	
 #ifdef ConsoleOut
 	#define out cout
+	#define sout cout
 #else
 	#define out testFile
-	#ifdef MyMapType
-		ofstream out = ofstream("testResults - myMap.txt");
-	#else
-		ofstream out = ofstream("testResults - stlMap.txt");
-	#endif
+	#define sout stestFile
+	ofstream out = ofstream("testResults - myMap.txt");
+	ofstream sout = ofstream("testResults - stlMap.txt");
+
 #endif
 
 inline string AsText(bool booleanValue)
@@ -45,43 +37,32 @@ inline string AsText(bool booleanValue)
 	}
 }
 
-int main()
+
+void Test1CMap(CMap<int, float> &myMap)
 {
-
-	MAP<int, float> myMap = MAP<int, float>();
-
-#ifdef MyMapType
-	out << "My CMap: " << endl << endl;
-#else
-	out << "STL Map: " << endl << endl;
-#endif
-	out << "Map Type: int, float" << endl;
-
 	///////////////////////////
 	out << "Insert 5 items" << endl;
 	for (int i = 1; i <= 5; i++)
 	{
 		out << "Input: " << i << ", " << i * 1.1f << endl;
-#ifdef MyMapType
 		myMap.insert(i, i * 1.1f);
-#else
-		myMap.insert(std::make_pair(i, i * 1.1f));
-#endif
 	}
 	out << "Current Size: " << myMap.size() << endl << endl;
-	
+}
+		  
+void Test2CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Find 5 items with function Find" << endl;
 	for (int i = 1; i <= 5; i++)
 	{
-#ifdef MyMapType
 		out << "Key: " << i << " Result: " << myMap.find(i) << endl;
-#else
-		out << "Key: " << i << " Result: " << (*myMap.find(i)).second << endl;
-#endif
 	}
 	out << "Current Size: " << myMap.size() << endl << endl;
-	
+}
+		  
+void Test3CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Find 5 items with operator[]" << endl;
 	for (int i = 1; i <= 5; i++)
@@ -89,45 +70,42 @@ int main()
 		out << "Key: " << i << " Result: " << myMap[i] << endl;		
 	}
 	out << "Current Size: " << myMap.size() << endl << endl;
-
+}
+		  
+void Test4CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Insert the same 5 keys with alternate values " << endl;
 	for (int i = 1; i <= 5; i++)
 	{
 		out << "Input: " << i;
-#ifdef MyMapType
 		myMap.insert(i, (i * 1.1f) + 1);
 		out << " Data: " << myMap.find(i) << endl;
-#else
-		myMap.insert(std::make_pair(i, (i * 1.1f) + 1));
-		out << " Data: " << (*myMap.find(i)).second << endl;
-#endif
 	}
 	out << "Current Size: " << myMap.size() << endl << endl;
 
+}
+		  
+void Test5CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Find 5 items with function Find" << endl;
 	for (int i = 1; i <= 5; i++)
 	{
-#ifdef MyMapType
 		out << "Key: " << i << " Result: " << myMap.find(i) << endl;
-#else
-		out << "Key: " << i << " Result: " << (*myMap.find(i)).second << endl;
-#endif
 	}
 	out << "Current Size: " << myMap.size() << endl << endl;
+}
+		  
+void Test6CMap(CMap<int, float> &myMap)
+{
 
 	///////////////////////////
 	out << "Remove 8 items (3 of which dont exist) with function remove" << endl;
 	float removedValue;
-#ifdef MyMapType
 	bool wasRemoved;
-#else
-	int wasRemoved;
-#endif
 	for (int i = 1; i <= 8; i++)
 	{
-#ifdef MyMapType
 		wasRemoved = myMap.remove(i, removedValue);
 		out << "Key: " << i << " Removed: " << AsText(wasRemoved);
 		if (wasRemoved)
@@ -135,135 +113,366 @@ int main()
 			out << " Value: " << removedValue;
 		}
 		out << endl;
-#else
-		
-		wasRemoved = myMap.erase(i);
-		out << "Key: " << i << " Removed: " << AsText(wasRemoved) << endl;
-#endif
 	}
 	out << "Current Size: " << myMap.size() << endl << endl;
-
+}
+		  
+void Test7CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Insert 5 items (with function find)" << endl;
-#ifdef MyMapType
 	for (int i = 1; i <= 5; i++)
 	{
 		out << "Input: " << i << ", " << i * 1.1f << endl;
 		myMap.find(i) = i * 1.1f;
 	}
-#else
-	//(*myMap.find(i)).second = i * 1.1f;
-	out << "Not allowed in stl implementation" << endl;
-	out << "Refill the array using insert instead" << endl;
-	for (int i = 1; i <= 8; i++)
-	{
-		myMap.insert(std::make_pair(i, i * 1.1f));
-	}
-#endif
 	out << "Current Size: " << myMap.size() << endl << endl;
 
+}
+		  
+void Test8CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Add 3 to the value of 8 items (with function find) where 3 items don't exist" << endl;
-#ifdef MyMapType
 	for (int i = 1; i <= 8; i++)
 	{
 		out << "Key: " << i;
 		myMap.find(i) += 3;
 		out << " Value: " << myMap.find(i) << endl;
 	}
-#else
-		//out << " Value: " << (*myMap.find(i)).second << endl;
-		out << "Not allowed in stl implementation" << endl;
-		out << "Insert 3 new values instead" << endl;
-		for (int i = 5; i <= 8; i++)
-		{
-			myMap.insert(std::make_pair(i, 3.0f));
-		}
-#endif
 	out << "Current Size: " << myMap.size() << endl << endl;
-
+}
+		  
+void Test9CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Use iterator to find key and data of each item" << endl;
-#ifdef MyMapType
 	for (CIterator<int, float> iter = myMap.IterBegin(); iter != myMap.IterEnd(); iter++)
 	{
 		out << "Key: " << (*iter).item1 << " Value: " << (*iter).item2 << endl;
 	}
-#else
-	for (map<int, float>::iterator iter = myMap.begin(); iter != myMap.end(); iter++)
-	{
-		out << "Key: " << (*iter).first << " Value: " << (*iter).second << endl;
-	}
-#endif
 	out << "Current Size: " << myMap.size() << endl << endl;
+}
 
+void Test10CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Remove selected items (keys divisible by 3)" << endl;
+	float removedValue;
+	bool wasRemoved;
 	for (int i = 1; i <= 8; i++)
 	{
 		if (i % 3 == 0)
 		{
-#ifdef MyMapType
 			wasRemoved = myMap.remove(i, removedValue);
-#else
-			wasRemoved = myMap.erase(i);
-#endif
 			out << "Key: " << i << " Removed: " << AsText(wasRemoved) << endl;
 		}
 	}
 	out << "Current Size: " << myMap.size() << endl << endl;
-
+}
+		 
+void Test11CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Use iterator to find key and data of each item" << endl;
-#ifdef MyMapType
 	for (CIterator<int, float> iter = myMap.IterBegin(); iter != myMap.IterEnd(); iter++)
 	{
 		out << "Key: " << (*iter).item1 << " Value: " << (*iter).item2 << endl;
 	}
-#else
-	for (map<int, float>::iterator iter = myMap.begin(); iter != myMap.end(); iter++)
-	{
-		out << "Key: " << (*iter).first << " Value: " << (*iter).second << endl;
-	}
-#endif
 	out << "Current Size: " << myMap.size() << endl << endl;
-
+}
+		   
+void Test12CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Use clear function to empty the map" << endl;
 	myMap.clear();
 	out << "Current Size: " << myMap.size() << endl << endl;
-
+}
+		   
+void Test13CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Time to add 10,000 items to an empty map" << endl;
 	high_resolution_clock::time_point start = high_resolution_clock::now();
 	for (int i = 0; i < 10000; i++)
 	{
-#ifdef MyMapType
 		myMap.insert(i, i * 1.1f);
-#else
-		myMap.insert(std::make_pair(i, i * 1.1f));
-#endif
 	}
 	high_resolution_clock::time_point end = high_resolution_clock::now();
 	long long noOfMilli = duration_cast<milliseconds>(end - start).count();
 	out << "Time: " << noOfMilli << " MilliSeconds " << endl;
-
+}
+		   
+void Test14CMap(CMap<int, float> &myMap)
+{
 	///////////////////////////
 	out << "Time to add 100,000 items to an empty map" << endl;
-	start = high_resolution_clock::now();
+	high_resolution_clock::time_point start = high_resolution_clock::now();
 	for (int i = 0; i < 100000; i++)
 	{
-#ifdef MyMapType
 		myMap.insert(i, i * 1.1f);
-#else
-		myMap.insert(std::make_pair(i, i * 1.1f));
-#endif
 	}
-	end = high_resolution_clock::now();
-	noOfMilli = duration_cast<milliseconds>(end - start).count();
+	high_resolution_clock::time_point end = high_resolution_clock::now();
+	long long noOfMilli = duration_cast<milliseconds>(end - start).count();
 	out << "Time: " << noOfMilli << " MilliSeconds " << endl;
+}
+
+
+void Test1STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Insert 5 items" << endl;
+	for (int i = 1; i <= 5; i++)
+	{
+		sout << "Input: " << i << ", " << i * 1.1f << endl;
+		myMap.insert(std::make_pair(i, i * 1.1f));
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test2STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Find 5 items with function Find" << endl;
+	for (int i = 1; i <= 5; i++)
+	{
+		sout << "Key: " << i << " Result: " << (*myMap.find(i)).second << endl;
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test3STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Find 5 items with operator[]" << endl;
+	for (int i = 1; i <= 5; i++)
+	{
+		sout << "Key: " << i << " Result: " << myMap[i] << endl;
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test4STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Insert the same 5 keys with alternate values " << endl;
+	for (int i = 1; i <= 5; i++)
+	{
+		sout << "Input: " << i;
+		myMap.insert(std::make_pair(i, (i * 1.1f) + 1));
+		sout << " Data: " << (*myMap.find(i)).second << endl;
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+
+}
+
+void Test5STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Find 5 items with function Find" << endl;
+	for (int i = 1; i <= 5; i++)
+	{
+		sout << "Key: " << i << " Result: " << (*myMap.find(i)).second << endl;
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test6STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Remove 8 items (3 of which dont exist) with function remove" << endl;
+	float removedValue;
+	int wasRemoved;
+	for (int i = 1; i <= 8; i++)
+	{
+		wasRemoved = myMap.erase(i);
+		sout << "Key: " << i << " Removed: " << AsText(wasRemoved) << endl;
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test7STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Insert 5 items (with function find)" << endl;
+	//(*myMap.find(i)).second = i * 1.1f;
+	sout << "Not allowed in stl implementation" << endl;
+	sout << "Refill the array using insert instead" << endl;
+	for (int i = 1; i <= 8; i++)
+	{
+		myMap.insert(std::make_pair(i, i * 1.1f));
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+
+}
+
+void Test8STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Add 3 to the value of 8 items (with function find) where 3 items don't exist" << endl;
+	//sout << " Value: " << (*myMap.find(i)).second << endl;
+	sout << "Not allowed in stl implementation" << endl;
+	sout << "Insert 3 new values instead" << endl;
+	for (int i = 5; i <= 8; i++)
+	{
+		myMap.insert(std::make_pair(i, 3.0f));
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test9STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Use iterator to find key and data of each item" << endl;
+	for (map<int, float>::iterator iter = myMap.begin(); iter != myMap.end(); iter++)
+	{
+		sout << "Key: " << (*iter).first << " Value: " << (*iter).second << endl;
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test10STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Remove selected items (keys divisible by 3)" << endl;
+	float removedValue;
+	int wasRemoved;
+	for (int i = 1; i <= 8; i++)
+	{
+		if (i % 3 == 0)
+		{
+			wasRemoved = myMap.erase(i);
+			sout << "Key: " << i << " Removed: " << AsText(wasRemoved) << endl;
+		}
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test11STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Use iterator to find key and data of each item" << endl;
+	for (map<int, float>::iterator iter = myMap.begin(); iter != myMap.end(); iter++)
+	{
+		sout << "Key: " << (*iter).first << " Value: " << (*iter).second << endl;
+	}
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test12STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Use clear function to empty the map" << endl;
+	myMap.clear();
+	sout << "Current Size: " << myMap.size() << endl << endl;
+}
+
+void Test13STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Time to add 10,000 items to an empty map" << endl;
+	high_resolution_clock::time_point start = high_resolution_clock::now();
+	for (int i = 0; i < 10000; i++)
+	{
+		myMap.insert(std::make_pair(i, i * 1.1f));
+	}
+	high_resolution_clock::time_point end = high_resolution_clock::now();
+	long long noOfMilli = duration_cast<milliseconds>(end - start).count();
+	sout << "Time: " << noOfMilli << " MilliSeconds " << endl;
+}
+
+void Test14STLMap(map<int, float> &myMap)
+{
+	///////////////////////////
+	sout << "Time to add 100,000 items to an empty map" << endl;
+	high_resolution_clock::time_point start = high_resolution_clock::now();
+	for (int i = 0; i < 100000; i++)
+	{
+		myMap.insert(std::make_pair(i, i * 1.1f));
+	}
+	high_resolution_clock::time_point end = high_resolution_clock::now();
+	long long noOfMilli = duration_cast<milliseconds>(end - start).count();
+	sout << "Time: " << noOfMilli << " MilliSeconds " << endl;
+}
+
+int main()
+{
+
+	CMap<int, float> myMap = CMap<int, float>();
+
+	out << "My CMap: " << endl << endl;
+
+	out << "Map Type: int, float" << endl;
+
+	Test1CMap(myMap);
+	
+	Test2CMap(myMap);
+
+	Test3CMap(myMap);
+	
+	Test4CMap(myMap);
+	
+	Test5CMap(myMap);
+
+	Test6CMap(myMap);
+
+	Test7CMap(myMap);
+
+	Test8CMap(myMap);
+
+	Test9CMap(myMap);
+
+	Test10CMap(myMap);
+
+	Test11CMap(myMap);
+
+	Test12CMap(myMap);
+
+	Test13CMap(myMap);
+
+	Test14CMap(myMap);
 
 	out << endl;
+
+	//--------------------------------------------------------------
+
+	map<int, float> stlMap = map<int, float>();
+
+
+	sout << "STL Map: " << endl << endl;
+
+	sout << "Map Type: int, float" << endl;
+
+	Test1STLMap(stlMap);
+
+	Test2STLMap(stlMap);
+
+	Test3STLMap(stlMap);
+
+	Test4STLMap(stlMap);
+
+	Test5STLMap(stlMap);
+
+	Test6STLMap(stlMap);
+
+	Test7STLMap(stlMap);
+
+	Test8STLMap(stlMap);
+
+	Test9STLMap(stlMap);
+
+	Test10STLMap(stlMap);
+
+	Test11STLMap(stlMap);
+
+	Test12STLMap(stlMap);
+
+	Test13STLMap(stlMap);
+
+	Test14STLMap(stlMap);
+
+	sout << endl;
 
 	system("pause");
 
